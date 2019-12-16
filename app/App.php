5900660f59b0e559d419042d9dd418e17a0df42e
Donatas Piragis <donatas.piragis@gmail.com>
2019-12-16 17:04:50 +0200

    PCB-19

--- app/App.php
+++ app/App.php
@@ -1,11 +1,25 @@
 <?php
 namespace App;
 class App {
+    const INSTALL_FOLDER = '/pcb_final_project/public';
+    private static $request_url;
+    private static $controller;
     /**
      * @param Function call to start app
      * @return returns app with predifined starting elements
      */
     public static function start(){
-        echo 'Startuota struktura';
+
+        self::$request_url = str_replace(self::INSTALL_FOLDER, '', $_SERVER['REQUEST_URI']);
+        self::$request_url = ltrim( self::$request_url, '/');
+        self::$request_url = rtrim( self::$request_url, '/');
+        self::$request_url = explode('/', self::$request_url);
+
+        $controller = Route::getController(self::$request_url);
+
+        $g = 'App\\Controller\\'.$controller[0];
+
+        self::$controller = new $g;
+        self::$controller->{$controller[1]}(self::$request_url[2] ?? '');
     }
 }
---
+++ app/Route.php
@@ -0,0 +1,43 @@
+<?php
+namespace App;
+class Route {
+
+    /**
+     * @param Array ROUTE
+     * @return routes for controllers
+     */
+    const ROUTE = [
+        'front@index' => ['FrontPageController', 'index'],
+        'front@create' => ['FrontPageController', 'create'],
+        'front@store' => ['FrontPageController', 'store'],
+        'front@edit' => ['FrontPageController', 'edit'],
+        'front@update' => ['FrontPageController', 'update'],
+        'front@destroy' => ['FrontPageController', 'destroy'],
+        'examples@index' => ['ExamplesController', 'index'],
+        'examples@create' => ['ExamplesController', 'create'],
+        'examples@store' => ['ExamplesController', 'store'],
+        'examples@edit' => ['ExamplesController', 'edit'],
+        'examples@update' => ['ExamplesController', 'update'],
+        'examples@destroy' => ['ExamplesController', 'destroy'],
+        'topic@index' => ['TopicController', 'index'],
+        'topic@create' => ['TopicController', 'create'],
+        'topic@store' => ['TopicController', 'store'],
+        'topic@edit' => ['TopicController', 'edit'],
+        'topic@update' => ['TopicController', 'update'],
+        'topic@destroy' => ['TopicController', 'destroy'],
+    ];
+
+    /**
+     * @param Array $url
+     * @return url key
+     */
+    public static function getController($url){
+        $url_key = $url[0].'@'.$url[1];
+        if(!isset(self::ROUTE[$url_key])){
+            die('Klaida 1');
+        }
+
+        return self::ROUTE[$url_key];
+
+    }
+}
