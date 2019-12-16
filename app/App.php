94e96ecadeadd2b0432128f4b961300a8576c2e7
Donatas Piragis <donatas.piragis@gmail.com>
2019-12-16 16:34:27 +0200

    PCB-3

---
+++ app/App.php
@@ -0,0 +1,11 @@
+<?php
+namespace App;
+class App {
+    /**
+     * @param Function call to start app
+     * @return returns app with predifined starting elements
+     */
+    public static function start(){
+        echo 'Startuota struktura';
+    }
+}
