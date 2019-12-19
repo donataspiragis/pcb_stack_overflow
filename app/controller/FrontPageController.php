71487f1bd51f89da5cb4535a327b05004ed8c448
Ugnius <ugnius_staniulis@msn.com>
2019-12-19 07:52:26 +0200

    PCB-8 Fixed search
    Now Searches with tag and title together.

--- app/controller/FrontPageController.php
+++ app/controller/FrontPageController.php
@@ -18,6 +18,12 @@ class FrontPageController extends BaseController  {
         $tag = $request->request->get('tag');
         $title = $request->request->get('search');
         if($tag !='default') {
+            if($title != ''){
+                $typeoftag = $this->getData("SELECT id FROM languages WHERE Tag = '" . $tag . "'");
+                $typeoftag = $typeoftag[0]['id'];
+                $raw = $this->getData("SELECT * FROM topics WHERE DocTagId = $typeoftag AND LOCATE('".$title."', Title) > 0");
+                return $this->render('frontpage/index', ['data' => $raw]);
+            }
             $typeoftag = $this->getData("SELECT id FROM languages WHERE Tag = '" . $tag . "'");
             $typeoftag = $typeoftag[0]['id'];
             $raw = $this->getData("SELECT * FROM topics WHERE DocTagId = $typeoftag");
