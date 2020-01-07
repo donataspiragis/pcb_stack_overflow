4007e9b3b3bde58098355d48cf6574078de271d3
Ugnius <ugnius_staniulis@msn.com>
2020-01-07 09:45:52 +0200

    PCB-8
    FIXED FrontPage index function

--- app/controller/BaseController.php
+++ app/controller/BaseController.php
@@ -7,9 +7,15 @@ ini_set('display_startup_errors', 1);
 
 use Twig\Environment;
 use Twig\Loader\FilesystemLoader;
+use DataBase\Connection;
 
 class BaseController{
 
+protected $db;
+    public function __construct()
+    {
+        $this->db = new Connection();
+    }
     public function render($templateName, array $parameters = array())
     {
         $templateName = !empty($templateName) ? $templateName : 'error404';
@@ -20,6 +26,16 @@ class BaseController{
     {
         $twig = new Environment(new FilesystemLoader('../src/views'), array('autoescape' => false));
         echo $twig->render('error404.php', $parameters);
+    }
+     public function getData($string){
+
+
+        $data = [];
+        foreach ($this->db->openConnection()->query($string) as $row) {
+            $data[] = $row;
+        }
+        $this->db->closeConnection();
+        return $data;
     }
 }
 
--- app/controller/FrontPageController.php
+++ app/controller/FrontPageController.php
@@ -2,14 +2,65 @@
 
 namespace App\Controller;
 use App\Controller\BaseController;
-require '../database/dbconnect.php';
+use Symfony\Component\HttpFoundation\Request;
+
+//require '../database/dbconnect.php';
 class FrontPageController extends BaseController  {
 
-    public function index(){
-        $con = new \Connection();
-        $data = $con->openConnection()->query('SELECT * from languages');
-        $datatopic = $con->openConnection()->query("SELECT Title FROM topics WHERE title='java'");
-        echo $this->render('index',  ['data' => $data, 'topics' => $datatopic]);
+    public function index($tagName, $titleName){
+        
+        $tag = $tagName;
+        $title = $titleName;
+        if($tag !='default') {
+            if($title != ''){
+                $typeoftag = $this->getData("SELECT id FROM languages WHERE Tag = '" . $tag . "'");
+                $typeoftag = $typeoftag[0]['id'];
+                $raw = $this->getData("SELECT * FROM topics WHERE DocTagId = $typeoftag AND LOCATE('".$title."', RemarksHtml) > 0");
+                $raw = self::getTag($raw);
+                $pagination = self::getPaginationNumber($raw);
+                return $this->render('index', ['data' => $raw,'pag' => $pagination]);
+            }
+            $typeoftag = $this->getData("SELECT id FROM languages WHERE Tag = '" . $tag . "'");
+            $typeoftag = $typeoftag[0]['id'];
+            $raw = $this->getData("SELECT * FROM topics WHERE DocTagId = $typeoftag");
+            $raw = self::getTag($raw);
+            $pagination = self::getPaginationNumber($raw);
+            return $this->render('index', ['data' => $raw,'pag' => $pagination]);
+        }else {
+            if($title != ''){
+                $bytitle = $this->getData("SELECT * FROM topics WHERE  LOCATE('".$title."', RemarksHtml) > 0");
+                $bytitle = self::getTag($bytitle);
+                $pagination = self::getPaginationNumber($bytitle);
+                return $this->render('index', ['data' => $bytitle,'pag' => $pagination]);
+            }
+            return $this->render('index', ['data' => '',]);
+        }
+        return $this->render('index', ['data' => '',]);
     }
+
+
+    private function getTag($data){
+        foreach($data as $key=>$value){
+            $typetag = $this->getData("SELECT Tag FROM languages WHERE id = '" . $value['DocTagId'] . "'");
+            $typetag = $typetag[0]['Tag'];
+            $data[$key]['TagName'] = $typetag;
+        }
+        return $data;
+
+    }
+    private function getPaginationNumber($data){
+        $pagination = count($data);
+        if($pagination != 0){
+            if(strlen($pagination) > 1){
+                if($pagination % 10 > 0){
+                    return (($pagination - $pagination % 10) / 10) + 1;
+                }else {
+                    return (($pagination - $pagination % 10) / 10);
+                }
+            }else return 0;
+        } else return 0;
+
+    }
+
 
 }
