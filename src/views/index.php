e472bf320e98f29b124427ada939789577fc3d9d
Ugnius <ugnius_staniulis@msn.com>
2020-01-07 16:00:03 +0200

    PCB-8
    import script

--- src/views/index.php
+++ src/views/index.php
@@ -8,35 +8,6 @@
                 <div class="card-header"><h1>Search Your Answer</h1></div>
 
                 <div class="card-body">
-                    <form action="/PCB/public/front/store" method="post">
-                        <select name="tag">
-                            <option value="default">Select Languege</option>
-                            <option value="java">Java</option>
-                            <option value="python">Python</option>
-                            <option value="php">PHP</option>
-                            <option value="c#">C#</option>
-                            <option value="javascript">Javascript</option>
-                            <option value="ruby">Ruby</option>
-                        </select>
-                        <input type="text" name = "search" placeholder="Search..">
-                        <button type="submit">Search</button>
-                    </form>
-                </div>
-            </div>
-        </div>
-    </div>
-</div>
-<div class="container">
-{% extends 'layout.php' %}
-{% block title %}Home{% endblock %}
-{% block body %}
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header"><h1>Search Your Answer</h1></div>
-
-                <div class="card-body">
                     <form action="/PCB/public" method="get">
                         <select name="tag">
                             <option value="default">Select Languege</option>
