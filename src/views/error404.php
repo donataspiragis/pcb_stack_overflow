1b66a5c72ff8afe7495060829f0bf5d5d3293c71
Donatas Piragis <donatas.piragis@gmail.com>
2020-01-08 09:39:18 +0200

    Error page update.

--- src/views/error404.php
+++ src/views/error404.php
@@ -1,5 +1,26 @@
 {% extends 'layout.php' %}
 
 {% block body %}
-    <h1>OMG! 404 Page not found</h1>
+
+
+<div class="container" style="text-align: center; margin-top: 50px;">
+    <div class="row">
+        <div class="col-md-12">
+            <div class="error-template">
+                <h1>
+                    Oops!</h1>
+                <h2>
+                    404 Not Found</h2>
+                <div class="error-details">
+                    Sorry, an error has occured, Requested page not found!
+                </div>
+                <div class="error-actions">
+                    <a href="{{ constant('App\\App::INSTALL_FOLDER') }}" class="btn btn-primary btn-lg"><span class="glyphicon glyphicon-home"></span>
+                        Take Me Home </a>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
 {% endblock %}
+
