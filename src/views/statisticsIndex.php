dade7d90e6f7606d61fc1186e6a56feb0de46287
ned <>
2020-01-08 09:47:54 +0200

    Statistics view layout changed to bootstrap table.

--- src/views/statisticsIndex.php
+++ src/views/statisticsIndex.php
@@ -12,13 +12,26 @@
 </div>
 
 <div id="stat-results-container" class="container">
-{% for topic in topics %}
-    <div class="container">
-        <p style="display: inline-block; width: 50px; color: red;">{{topic.ViewCount}}</p>
-        <a href="{{ constant('App\\App::INSTALL_FOLDER') }}/topic/index/{{topic.DocTagId}}" style="display: inline-block; font-weight: bolder;">{{topic.Title}}</a>
-        <p style="display: inline-block; margin-left: 5px; color: green;">{{topic.Language}}</p>
-    </div>
-{% endfor %}
+    <table class="table table-striped">
+        <thead>
+            <tr>
+                <th scope="col">#</th>
+                <th scope="col">ViewCount</th>
+                <th scope="col">Topic</th>
+                <th scope="col">Language</th>
+            </tr>
+        </thead>
+        <tbody>
+            {% for index, topic in topics %}
+                <tr>
+                    <th scope="row">{{ index + 1 }}</th>
+                    <td>{{ topic.ViewCount }}</td>
+                    <td><a href="{{ constant('App\\App::INSTALL_FOLDER') }}/topic/index/{{topic.DocTagId}}">{{ topic.Title }}</a></td>
+                    <td>{{ topic.Language }}</td>
+                </tr>
+            {% endfor %}
+        </tbody>
+    </table>
 </div>
 {% endblock %}
 
